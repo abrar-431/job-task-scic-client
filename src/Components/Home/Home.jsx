825c9bf518ef0a2cbd0ff08ef04e5d8e6eb6ad17
abrar-431 <abrarh1520444@gmail.com>
2024-08-18 02:26:02 -0700

    filter added all

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,18 +5,22 @@ import { useLoaderData } from 'react-router-dom';
 const Home = () => {
     const totalProducts = useLoaderData()
     const distinctBrands = [...new Set(totalProducts.map(product => product.brandName))];
+    const distinctCategories = [...new Set(totalProducts.map(product => product.category))];
     const [products, setProducts] = useState([]);
     const totalItems = totalProducts.length;
     const [totalItemsPerPage, setTotalItemsPerPage] = useState(5);
     const [currentPage, setCurrentPage] = useState(1);
     const [brand, setBrand] = useState('');
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(1000);
+    const [categoryName, setCategoryName] = useState('');
     const totalPages = Math.ceil(totalItems / totalItemsPerPage);
     const pages = [];
     useEffect(() => {
-        fetch(`http://localhost:5000/products?page=${currentPage}&items=${totalItemsPerPage}&brand=${brand}`)
+        fetch(`http://localhost:5000/products?page=${currentPage}&items=${totalItemsPerPage}&brand=${brand}&categoryName=${categoryName}&minPrice=${minPrice}&maxPrice=${maxPrice}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [currentPage, totalItemsPerPage, brand])
+    }, [currentPage, totalItemsPerPage, brand, categoryName, minPrice, maxPrice])
     const handlePrevButton = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1)
@@ -35,18 +39,71 @@ const Home = () => {
     for (let i = 1; i <= totalPages; i++) {
         pages.push(i);
     }
-    const handleBrandName = e =>{
+    const handleBrandName = e => {
         setBrand(e.target.value);
     }
-    console.log(brand)
+    const handleCategoryName = e => {
+        setCategoryName(e.target.value);
+    }
+    const handlePriceRange = e => {
+        const opt = e.target.value;
+        switch (opt) {
+            case 'a':
+                setMinPrice(0);
+                setMaxPrice(20);
+                break;
+            case 'b':
+                setMinPrice(21);
+                setMaxPrice(50);
+                break;
+            case 'c':
+                setMinPrice(51);
+                setMaxPrice(80);
+                break;
+            case 'd':
+                setMinPrice(81);
+                setMaxPrice(150);
+                break;
+            case 'e':
+                setMinPrice(151);
+                setMaxPrice(200);
+                break;
+            case 'f':
+                setMinPrice(201);
+                setMaxPrice(1000);
+                break;
+            default:
+                setMinPrice(0);
+                setMaxPrice(1000);
+        }
+    }
+    console.log(minPrice, maxPrice)
     return (
         <div>
             {/* Category According to Brand Name */}
             <select onChange={handleBrandName} defaultValue='' className="select select-info w-full max-w-xs">
                 <option disabled value=''>Select Brand Name</option>
                 {
-                    distinctBrands.map((brand, idx)=><option key={idx} value={brand}>{brand}</option>)
+                    distinctBrands.map((brand, idx) => <option key={idx} value={brand}>{brand}</option>)
                 }
+            </select>
+            {/* Categorization According to Category Name */}
+            <select onChange={handleCategoryName} defaultValue='' className="select select-info w-full max-w-xs">
+                <option disabled value=''>Select Category Name</option>
+                {
+                    distinctCategories.map((category, idx) => <option key={idx} value={category}>{category}</option>)
+                }
+            </select>
+            {/* Price Range Categorization */}
+            <select onChange={handlePriceRange} defaultValue='' className="select select-info w-full max-w-xs">
+                <option disabled value=''>Price Range</option>
+                <option value='a'>0-20</option>
+                <option value='b'>21-50</option>
+                <option value='c'>51-80</option>
+                <option value='d'>81-150</option>
+                <option value='e'>151-200</option>
+                <option value='f'>Above 200</option>
+                <option value='h'>All</option>
             </select>
             <Products products={products}></Products>
             <div className='flex justify-center'>
